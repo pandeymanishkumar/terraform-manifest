730dbb4d677b4c5ae2cd6a3f832535f0d297ab24
Manish <manish.kp@live.com>
2025-04-15 08:13:37 +0530

    adding backend

--- provider.tf
+++ provider.tf
@@ -7,6 +7,15 @@ terraform {
   }
 }
 
+# Terraform State Storage to Azure Storage Container
+  backend "azurerm" {
+    resource_group_name   = "statefile-rg"
+    storage_account_name  = "mystatefile810003596"
+    container_name        = "statfile"
+    key                   = "terraform.tfstate"
+  } 
+
 provider "azurerm" {
   features {}
 }
+
