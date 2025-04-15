ab6eeb2853118d0a725a9608e6166d20f9a9440f
Manish <manish.kp@live.com>
2025-04-15 08:27:33 +0530

    changes done on main.tf

--- provider.tf
+++ provider.tf
@@ -1,21 +1,19 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.26.0"
     }
   }
-}
 
-# Terraform State Storage to Azure Storage Container
   backend "azurerm" {
     resource_group_name   = "statefile-rg"
     storage_account_name  = "mystatefile810003596"
     container_name        = "statfile"
     key                   = "terraform.tfstate"
-  } 
+  }
+}
 
 provider "azurerm" {
   features {}
 }
-
